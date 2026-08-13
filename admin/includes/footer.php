28224a95e3e07e18dad745808d0f789fc689c0f1
aayu_bas <basnetaayusha102@gmail.com>
2026-08-13 06:39:29 +0545

    implemented footer file for admin page

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+<?php
+?>
+            </div><!-- admin-content -->
+        </main><!-- admin-main -->
+    </div><!-- admin-wrapper -->
+
+    <script src="<?php echo ADMIN_ASSETS_URL; ?>/js/admin.js"></script>
+    <?php if (isset($adminExtraJS)) echo $adminExtraJS; ?>
+</body>
+</html>
